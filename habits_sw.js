bfa87f4749efd244e06a36647acf58a7833309bb
samratpatel <samrat.patel@gmail.com>
2026-04-28 23:12:23 -0500

    Add files via upload

---
+++ habits_sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'habits-v1';
+const ASSETS = ['./habits.html', './habits_manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
+
+self.addEventListener('notificationclick', e => {
+  e.notification.close();
+  e.waitUntil(clients.openWindow('./habits.html'));
+});
